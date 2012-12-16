14ada5d975caf7063efea313c8cab468d2afedb4
Alexandre Pallueau <alexandre.pallueau@sfr.fr>
2012-12-16 12:24:25 +0100

    J'AI TROUVE COMMENT CHARGER LE CSS SUR UN ELEMENT CREE DYNAMIQUEMENT

--- Appli/assets/www/javascript/chargement.js
+++ Appli/assets/www/javascript/chargement.js
@@ -2,6 +2,22 @@ function onLoad(){
 	document.addEventListener("deviceready", onDeviceReady, false);
 }
 
-function onDeviceReady(){
+//$(document).ready(function(){onDeviceReady()});
 
+function onDeviceReady(){
+	$.ajax({
+		url: 'html/connexion.html',
+		dataType: 'html',
+		success: function(data){
+			$("#contener").append(data);
+			//$("#page *").page();
+			//$('#header').header('refresh');
+			//$("#contener").empty().append( options ).selectmenu( "refresh", true );
+			// $.mobile.initializePage();
+			$("#page").page("destroy").page();
+		},
+		error: function(){
+			alert("Erreur: formulaire de connexion non chargé");
+		}
+	});
 }
