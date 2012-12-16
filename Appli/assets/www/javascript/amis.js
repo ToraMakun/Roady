a280361e6eb762c07890633aa20bccff02252c97
Alexandre Pallueau <alexandre.pallueau@sfr.fr>
2012-12-16 16:39:06 +0100

    Essai popup amis.html

--- Appli/assets/www/javascript/amis.js
+++ Appli/assets/www/javascript/amis.js
@@ -1,7 +1,7 @@
 function instanciationAjoutAmi(){
 
 	$("[data-role=slider").on('slidestart', function(event) {alert("Valeur change")});
-	$(".groupName").on('taphold', function(){$("#gestionGroupe").popup("open");});
+	$(".groupName").on('taphold', function(){$("#gestionGroupe").popup("open", {x:0, y:0});});
 	$(".ui-block-a").on('tap', afficherFicheAmi);
 	
 	$("#boutonRetour").text("Retour");
