104efc601f8d1c7e392f9cbe3d656489cb9874e5
Alexandre Pallueau <alexandre.pallueau@sfr.fr>
2012-12-17 18:20:19 +0100

    Tentative centrer carte: fonctionne sous firefox, pas sur chrome ou simulateur

--- Appli/assets/www/javascript/map.js
+++ Appli/assets/www/javascript/map.js
@@ -1,23 +1,36 @@
+var map;
+var latLng;
+
 function instanciationMap(){
 
-	//initialiserCarte();
-	
 	$("#boutonRetour").text("Retour");
 	$("#boutonRetour").unbind();
 	$("#boutonRetour").click(function(){afficherMenu();});
 }
 
-var map;
-var initialize;
- 
+function centrerCarte(){
+	alert("test");
+	navigator.geolocation.getCurrentPosition(function(position) {alert("bon");
+	latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);},
+	function(error) {alert("pas bon");
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+		});
+	alert("test2");
+	map.setCenter(latLng);
+}
+
 function initialiserCarte(){
-  var latLng = new google.maps.LatLng(50.6371834, 3.063017400000035); // Correspond au coordonnées de Lille
-  var myOptions = {
-    zoom      : 14,
-    center    : latLng,
-    mapTypeId : google.maps.MapTypeId.TERRAIN, // Type de carte, différentes valeurs possible HYBRID, ROADMAP, SATELLITE, TERRAIN
-    maxZoom   : 20
-  };
- 
-  map      = new google.maps.Map(document.getElementById('map'), myOptions);
+	
+	latLng = new google.maps.LatLng(50.6371834, 3.063017400000035);
+	
+	var myOptions = {
+	    zoom      : 14,
+	    center    : latLng,
+	    mapTypeId : google.maps.MapTypeId.TERRAIN, // Type de carte, différentes valeurs possible HYBRID, ROADMAP, SATELLITE, TERRAIN
+	    maxZoom   : 20
+	};
+	 
+	map = new google.maps.Map(document.getElementById('map'), myOptions);
+	centrerCarte();
 }
